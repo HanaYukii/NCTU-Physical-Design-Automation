f8f44b3d025cd6ab2da3e4985c208c984a41d202
HanaYukii <islu245777@gmail.com>
2020-11-26 17:05:47 +0800

    pass testcases

--- Lab3/Lab3-sequence.cpp
+++ Lab3/Lab3-sequence.cpp
@@ -8,14 +8,19 @@ using namespace std;
 #define F first
 #define S second
 #define all(x) x.begin(), x.end()
-const int maxn = 1e4;
+const int maxn = 105;
+double T = 4000000000;
+double r = 0.85;
 vector<pair<int,int>>blocks;
 vector<int>pos;
 vector<int>neg;
 int n;
 int x[maxn], rx[maxn], y[maxn], ry[maxn];
+int ansx[maxn], ansrx[maxn], ansy[maxn], ansry[maxn]; 
 int lz[maxn<<2];
 int sum[maxn<<2];
+int Best_area, H, W, limx, limy;
+double Last_cost; 
 void pull(int x,int l,int r) {
     if (lz[x]) {
         sum[x] = r - l + 1;
@@ -56,7 +61,6 @@ void check() {
     sort(all(e));
     for (auto &i : e) {
         auto [pos, l, r, d] = i;
-        //cout << pos <<' '<<l<<' '<<r<<' '<<d<<endl;
         if (pos != last) {
             ans += 1LL * sum[1] * (pos - last);
         }
@@ -68,13 +72,11 @@ void check() {
         tot_area += i.F * i.S;
     }
     cout <<ans <<' '<<tot_area<<endl;
-    //assert(ans == tot_area);
 }
 void cal() {
     int idx[n] = {};
-    f(n) {
+    for (int i = 0 ; i < n ; i++) {
         idx[pos[i]] = i;
-
     }
     int b[n + 1] = {};
     auto update = [&](int x,int val) -> void{
@@ -105,22 +107,132 @@ void cal() {
         ry[neg[i]] = prev + blocks[neg[i]].S;
         update(idx[neg[i]] + 1, ry[neg[i]]);
     }
-    check();
-    f(n) {
-        cout << i <<' '<<blocks[i].F <<' '<<blocks[i].S <<' '<<x[i] <<' '<<rx[i] <<' '<<y[i] << ' '<<ry[i] << endl;
-    }
+    reverse(all(neg));
 }
 void random_solution() {
+    Last_cost = 1e9;
     random_shuffle(all(pos));
     random_shuffle(all(neg));
+}
+int getarea(int &x,int &y) {
+    f1(n) {
+        x = max(x, rx[i]);
+        y = max(y, ry[i]);
+    }
+    return x * y;
+}
+void upd() {
+    int r = rand() % 100;
+    tuple<int,int,int>mov; 
+    int rot = -1;
+    if (r < 35) {
+        int idx = rand() % n;
+        rot = idx;
+        swap(blocks[idx].F, blocks[idx].S);    
+    }
+    if (r < 50) {
+        int n1 = rand() % n;
+        int n2 = rand() % n;
+        while (n2 == n1) {
+            n2 = rand() % n;
+        }
+        if (rand() & 1) {
+            mov = make_tuple(n1,n2,1);
+            swap(pos[n1], pos[n2]);
+        }
+        else {
+            mov = make_tuple(n1,n2,2);
+            swap(neg[n1], neg[n2]);
+        }
+    }
+    else {
+        int n1 = rand() % n;
+        int n2 = rand() % n;
+        while (n2 == n1) {
+            n2 = rand() % n;
+        }
+        mov = make_tuple(n1,n2,3);
+        swap(pos[n1], pos[n2]);
+        swap(neg[n1], neg[n2]);
+    }
     cal();
+    int X = 0, Y = 0;
+    int Area = getarea(X, Y);
+    double Ratio = (double)(X * limy) / (double)(Y * limx);
+    if (Ratio < 1) {
+        Ratio = 1 / Ratio;
+    }
+    if (X > limx) {
+        Ratio *= 1.10;
+    }
+    if (Y > limy) {
+        Ratio *= 1.10;
+    }
+    if (X <= limx && Y <= limy) {
+        Ratio *= 0.8;
+    }
+    double Cost = ((double)Area * Ratio);
+    bool force = (double)rand() / RAND_MAX < exp(-1 * (double)Cost / T);
+    if (Cost <= Last_cost || force) {
+        Last_cost = Cost;
+    }
+    else {
+        if (get<2>(mov) == 3) {
+            swap(pos[get<0>(mov)], pos[get<1>(mov)]);
+            swap(neg[get<0>(mov)], neg[get<1>(mov)]);
+        }
+        else if (get<2>(mov) == 2) {
+            swap(neg[get<0>(mov)], neg[get<1>(mov)]);
+        }
+        else {
+            swap(pos[get<0>(mov)], pos[get<1>(mov)]);
+        }
+        if (~rot) {
+            swap(blocks[rot].F, blocks[rot].S); 
+        }
+    }
+}
+void SA() {
+    T = 4000000000;
+    int iter = 2000000;
+    while (iter--) {
+        upd();
+        T *= r;
+    }
+}
+void update_final() {
+    int X = 0, Y = 0;
+    int Area = getarea(X, Y);
+    cout << X <<' '<<Y << endl;
+    if (X <= limx && Y <= limy && Area <= Best_area) {
+        H = X, W = Y;
+        for (int i = 0 ; i < n ; i++) {
+            ansx[i] = x[i];
+            ansrx[i] = rx[i];
+            ansy[i] = y[i];
+            ansry[i] = ry[i];
+        }
+        Best_area = Area;
+    }
 }
 int main() {
+    srand(time(NULL));
+    Best_area = 2e9;
+    cin >> limx >> limy;
     cin >> n;
-    f(n) {
+    for (int i = 0 ; i < n ; i++) {
         pos.pb(i);
         neg.pb(i);
-        blocks.pb({rand() % 50 + 1, rand() % 50 + 1});
+        int x, y;
+        cin >> x >> y;
+        blocks.pb({x, y});
     }
-    random_solution();
+    int SA_time = 10;
+    H = W = 1e9;
+    while (SA_time--) {
+        random_solution();
+        SA();
+        update_final();
+        cout << H << ' ' << W << endl;
+    }
 }
